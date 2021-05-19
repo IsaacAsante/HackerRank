a821473275b951e37893c90b2f11fc8bf39e72e1
Isaac Asante <isaac.editor16@gmail.com>
2021-05-19 11:59:22 +0800

    Publishing the solution for 'Making Anagrams' for a YouTube community post.

---
+++ Problem Solving/Algorithms/Strings/Making Anagrams/making anagrams.cpp
@@ -0,0 +1,27 @@
+// TODO: Record the video for the "Making Anagrams" challenge in both C and C++.
+
+/* Author: Isaac Asante
+ * HackerRank URL for this exercise: https://www.hackerrank.com/challenges/making-anagrams/problem
+ * Original video explanation: Pending
+ * Last verified on: May 19, 2021
+ */
+
+ /* IMPORTANT:
+  * This code is meant to be used as a solution on HackerRank (link above).
+  * It is not meant to be executed as a standalone program.
+  */
+
+int makingAnagrams(string s1, string s2) {
+    char alphabet[26] = { 0 }; // All counts initialized to zero
+    int sum = 0;
+    for (int i = 0; i < s1.size(); i++)
+        --alphabet[s1[i] - 'a']; // Decrease for s1
+
+    for (int i = 0; i < s2.size(); i++)
+        ++alphabet[s2[i] - 'a']; // Increase for s2
+
+    for (int i = 0; i < 26; i++)
+        sum += abs(alphabet[i]); // Get the sum of abs values
+
+    return sum;
+}
